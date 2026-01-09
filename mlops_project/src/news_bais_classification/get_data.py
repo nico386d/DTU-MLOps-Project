07d7ea8980809fba8b5037e65f97955f3b873623
Mathias Herløv Lund <s204696@dtu.dk>
2026-01-09 10:42:46 +0100

    Updated Requirements.txt, created data folder and created get_data.py script

---
+++ mlops_project/src/news_bais_classification/get_data.py
@@ -0,0 +1,21 @@
+#This script downloads the latest version of the Brain MRI Images for Brain Tumor Detection dataset from Kaggle.
+import shutil
+from pathlib import Path
+
+import kagglehub
+
+# Define the target directory
+RAW_DATA_DIR = Path(__file__).parent.parent.parent.parent / "data" / "raw"
+
+# Download latest version
+cache_path = kagglehub.dataset_download("navoneel/brain-mri-images-for-brain-tumor-detection")
+
+# Copy to data/raw folder
+RAW_DATA_DIR.mkdir(parents=True, exist_ok=True)
+target_path = RAW_DATA_DIR / "brain-mri-images"
+
+if target_path.exists():
+    shutil.rmtree(target_path)
+shutil.copytree(cache_path, target_path)
+
+print("Dataset downloaded to:", target_path)
