ddf83806da31d047275ac8c865da48e137b03d8f
Mathias Herløv Lund <s204696@dtu.dk>
2026-01-09 11:30:13 +0100

    Switched to new Chest CT-Scan images Dataset from kaggle

--- mlops_project/src/news_bais_classification/get_data.py
+++ mlops_project/src/news_bais_classification/get_data.py
@@ -1,4 +1,4 @@
-#This script downloads the latest version of the Brain MRI Images for Brain Tumor Detection dataset from Kaggle.
+#This script downloads the latest version of the Chest CT-Scan Images dataset from Kaggle.
 import shutil
 from pathlib import Path
 
@@ -8,11 +8,11 @@ import kagglehub
 RAW_DATA_DIR = Path(__file__).parent.parent.parent / "data" / "raw"
 
 # Download latest version
-cache_path = kagglehub.dataset_download("navoneel/brain-mri-images-for-brain-tumor-detection")
+cache_path = kagglehub.dataset_download("mohamedhanyyy/chest-ctscan-images")
 
 # Copy to data/raw folder
 RAW_DATA_DIR.mkdir(parents=True, exist_ok=True)
-target_path = RAW_DATA_DIR / "brain-mri-images"
+target_path = RAW_DATA_DIR / "chest-ctscan-images"
 
 if target_path.exists():
     shutil.rmtree(target_path)
